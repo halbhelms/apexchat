c68d54ade2ee143ba05b006d5c3a6422db563091
Hal Helms <hal.helms@gmail.com>
2021-01-03 13:26:55 -0800

    03Jan1330

---
+++ src/components/leads/Contact.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="contact" v-if="lead">
+        <div class="card">
+            <!-- contact name -->
+            <div class="contact-element">
+                <div class="name-label"></div>
+                <div class="name-data"></div>
+            </div>
+            <!-- contact address -->
+            <div class="contact-element">
+                <div class="address-label"></div>
+                <div class="address-data"></div>
+            </div>
+            <!-- contact phone -->
+            <div class="contact-element">
+                <div class="phone-label"></div>
+                <div class="phone-data"></div>
+            </div>
+            <!-- contact email -->
+            <div class="contact-element">
+                <div class="email-label"></div>
+                <div class="email-data"></div>
+            </div>
+        </div>
+    </div>
+    <div v-else>Contact</div>
+</template>
+
+<script>
+    export default {
+        name: 'Contact',
+        components: {},
+        props: {
+            lead: {
+                required: false
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {},
+        computed: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
