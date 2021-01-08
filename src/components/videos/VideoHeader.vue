d1123ffb6ce81be178a15a637f0592126e12d5e6
Hal Helms <hal.helms@gmail.com>
2021-01-08 09:38:24 -0800

    Videos working with notifications

---
+++ src/components/videos/VideoHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="header">
+        <div class="title">Videos</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'VideoHeader',
+        components: {},
+        props: {},
+        data() {
+            return {}
+        },
+
+        computed: {}
+    }
+</script>
+
+<style scoped>
+
+    .header {
+        box-shadow: 0 0 6px 0 silver;
+        background-color: rgb(237,240,245);
+    }
+
+    .title {
+        font-size: 1.5rem;
+        font-weight: bold;
+        text-align: left;
+        padding-left: 20px;
+    }
+</style>
--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,5 +1,5 @@
 <template>
-    Videos
+    <VideoHeader />
     <div class="video-thumbnails">
         <Video v-for="(embedCode, index) in $store.state.videos" :key="index" :_embedCode="embedCode" />
 
@@ -9,9 +9,10 @@
 
 <script>
     import Video from '../components/videos/Video'
+    import VideoHeader from '../components/videos/VideoHeader'
     export default {
         name: 'Videos',
-        components: {Video},
+        components: {Video, VideoHeader},
         props: [],
         data() {
             return {}
@@ -25,6 +26,7 @@
     .video-thumbnails {
         margin-left: 20px;
         margin-right: 20px;
+        margin-top: 12px;
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
         grid-gap: 12px;
