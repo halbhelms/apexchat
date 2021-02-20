5e25aeed0fb618497c5cd255f945366368519a24
Hal Helms <hal.helms@gmail.com>
2021-02-19 21:19:52 -0800

    LoginForm skeleton done

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+<div v-if='inDev' class='inDev'>{{ options.name}}</div>
+</template>
+
+<script>
+    export default {
+        name: 'LoginForm',
+
+        components: {},
+
+        props: [],
+
+        data() {
+            return {}
+        },
+
+        methods: {},
+
+        computed: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
