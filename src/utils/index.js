a68a3b42634bdafe3aa259ceaf5beec91a0f5a60
Hal Helms <hal.helms@gmail.com>
2021-03-05 16:22:37 -0800

    Dashboard Graph completed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 const dateToApiDateString = (dateString) => {
     const date = new Date(dateString)
     const year = date.getFullYear()
-    const month = date.getMonth()
+    const month = date.getMonth() + 1
     const day = date.getDate()
     return `${year}-${month}-${day}`
 }
