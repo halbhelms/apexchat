2d781587c2f17b8bf35a2ba1cce1f89f4b3d4fe4
Hal Helms <hal.helms@gmail.com>
2021-04-28 13:17:50 -0700

    Login styling complete

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,25 +1,21 @@
 <template>
-    <section class="">
+    <section class="login-form-wrapper">
         <div v-if='inDev' class='inDev'>{{ $options.name}}</div>
         <section-header>Login</section-header>
         <p class="error" v-if="$store.state.loginError">We couldn't log you in. Please try again or contact your Moger Media representative.</p>
         <form class="login-form" @submit.prevent="loginUser">
-            <base-input 
-                _label="email" 
-                v-model.trim="login.email" 
-                _id="email">
-            </base-input>
-            <base-input 
-                _label="Password" 
-                _type="password" 
-                v-model.trim="login.password" 
-                _id="password">
-            </base-input>
-            <base-button 
-                :_styles="styles.submitButton" 
-                _type="submit">
-                Login
-            </base-button>
+            <!-- email -->
+            <div class="form-control">
+                <input v-model.trim="login.email" id="email" />
+                <label for="email">Email</label>
+            </div>
+            <!-- password -->
+            <div class="form-control">
+                <input type="password" v-model.trim="login.password" id="password" />
+                <label for="password">Password</label>
+            </div>
+            
+            <button type="submit" class="submit-button">Login</button>
         </form>
     </section>
 </template>
@@ -73,9 +69,46 @@
 </script>
 
 <style scoped>
-    .login-form {
+    .login-form-wrapper {
         position: relative;
-        width: 400px;
+        width: 100vw;
         margin: 0 auto;
+        z-index: 200;
+    }
+
+    .login-form {
+        /* position: relative; */
+        width: 500px;
+        margin: 0 auto;
+        z-index: 199;
+        /* left: 350px; */
+    }
+
+    .form-control {
+        margin-top: 24px;
+    }
+
+    input {
+        border: 1px solid silver;
+        font-size: 1.4rem;
+        position: relative;
+        width: 320px;
+    }
+
+    label {
+        display: block;
+        background: rgb(21,118,227);
+        color: white;
+        width: 320px;
+        margin: 0 auto;
+    }
+
+    button {
+        width: 200px;
+        height: 40px;
+        background: rgb(21,118,227);
+        color: white;
+        border-radius: 20px;
+        margin-top: 24px;
     }
 </style>
